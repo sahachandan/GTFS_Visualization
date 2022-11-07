a16e056f00737624d5c9add1b9a217ae59da5285
Chandan Saha <saha.chandan94@gmail.com>
2022-11-07 23:22:18 +0530

    comment updated

--- 02_script/visualize_routes_without_shapes_txt.py
+++ 02_script/visualize_routes_without_shapes_txt.py
@@ -209,6 +209,7 @@ m = folium.Map(
     zoom_start = 16,
     control_scale = True)
 
+# Feature group to store layer
 shape_Layer = folium.FeatureGroup(name='shapes_geom').add_to(m)
 stops_Layer = folium.FeatureGroup(name='stops_geom').add_to(m)
 
